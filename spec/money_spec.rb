d67c1ea0b354b6287731f267e8b14cc6af015d87
Yoshinori Tokuno <yoshi.jpn.10.11@gmail.com>
2021-09-24 09:00:47 +0900

    第2章完了

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -3,7 +3,9 @@ require_relative '../lib/money'
 RSpec.describe "MoneyTest" do
   it "test multiplication" do
     five = Dollar.new(5)
-    five.times(2)
-    expect(five.amount).to eq 10
+    product = five.times(2)
+    expect(product.amount).to eq 10
+    product = five.times(3)
+    expect(product.amount).to eq 15
   end
 end
